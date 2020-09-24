4146a0fb5442343bff1f0df162ed4c1a59ea6fdf
Anuradha <anuradhakhadanga6@gmail.com>
2020-09-24 17:34:14 +0200

    created cart POJO

--- src/com/product/core/CartItem.java
+++ src/com/product/core/CartItem.java
@@ -2,4 +2,19 @@ package com.product.core;
 
 public class CartItem {
 
+	private String skuId;
+	public String getSkuId() {
+		return skuId;
+	}
+	public void setSkuId(String skuId) {
+		this.skuId = skuId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	private int quantity;
+	
 }
