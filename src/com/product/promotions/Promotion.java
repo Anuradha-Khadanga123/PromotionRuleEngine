458ae08cc363282d32d1646d4412cdc84bc93914
Anuradha <anuradhakhadanga6@gmail.com>
2020-09-24 17:55:21 +0200

    Generated stubs for Promotion

--- src/com/product/promotions/Promotion.java
+++ src/com/product/promotions/Promotion.java
@@ -1,5 +1,29 @@
 package com.product.promotions;
 
+import java.util.List;
+
 public class Promotion {
+	
+	private List<PromotionCriteria> criterias;
+	private double discountedPrice;
+	
+	public Promotion(List<PromotionCriteria> criterias, double discountedPrice) {
+		super();
+		this.criterias = criterias;
+		this.discountedPrice = discountedPrice;
+	}
+	public List<PromotionCriteria> getCriterias() {
+		return criterias;
+	}
+	public void setCriterias(List<PromotionCriteria> criterias) {
+		this.criterias = criterias;
+	}
+	public double getDiscountedPrice() {
+		return discountedPrice;
+	}
+	public void setDiscountedPrice(double discountedPrice) {
+		this.discountedPrice = discountedPrice;
+	}
+	
 
 }
