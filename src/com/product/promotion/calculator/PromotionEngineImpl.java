1f035842a022b67a66ac616d892a331665e271fa
Anuradha <anuradhakhadanga6@gmail.com>
2020-09-24 18:03:06 +0200

    Created an Impl class to handle Promotions

---
+++ src/com/product/promotion/calculator/PromotionEngineImpl.java
@@ -0,0 +1,17 @@
+package com.product.promotion.calculator;
+
+import java.util.List;
+
+import com.product.core.CartItem;
+import com.product.core.Product;
+import com.product.promotions.Promotion;
+
+public class PromotionEngineImpl implements PromotionEngine{
+
+	@Override
+	public double getDiscountPriceOnCart(List<CartItem> cart, List<Promotion> promotions, List<Product> products) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
