20bd89ac7c56f80474acdbca7c298d30361c7860
Anuradha <anuradhakhadanga6@gmail.com>
2020-09-24 18:59:16 +0200

    Added list of available products

--- src/com/product/promotion/calculator/test/PromotionEngineImplTest.java
+++ src/com/product/promotion/calculator/test/PromotionEngineImplTest.java
@@ -1,5 +1,20 @@
 package com.product.promotion.calculator.test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.product.core.Product;
+import com.product.promotion.calculator.PromotionEngineImpl;
+
 public class PromotionEngineImplTest {
+	
+	PromotionEngineImpl promotions = new PromotionEngineImpl();
+	/*
+	 * List of available products
+	 */
+	List<Product> products = Arrays.asList(new Product("A",50), new Product("B",30), new Product("C",20),new Product("D",15));
+	
+	
+	
 
 }
