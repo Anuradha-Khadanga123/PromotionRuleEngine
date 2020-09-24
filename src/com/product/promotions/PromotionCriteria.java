6ce5a19357ed5026c7178d018554aec98c5719b6
Anuradha <anuradhakhadanga6@gmail.com>
2020-09-24 17:47:47 +0200

    Generated stubs for PromotionCriteria

--- src/com/product/promotions/PromotionCriteria.java
+++ src/com/product/promotions/PromotionCriteria.java
@@ -2,7 +2,26 @@ package com.product.promotions;
 
 public class PromotionCriteria {
 	
+	private int quantity;
+	private String skuId;
 	
+	public PromotionCriteria(int quantity, String skuId) {
+		super();
+		this.quantity = quantity;
+		this.skuId = skuId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getSkuId() {
+		return skuId;
+	}
+	public void setSkuId(String skuId) {
+		this.skuId = skuId;
+	}
 	
 	
 
