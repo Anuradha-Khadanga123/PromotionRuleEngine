29015389593b2e288d88fe9a0327258c262bb7ce
Anuradha <anuradhakhadanga6@gmail.com>
2020-09-24 17:39:25 +0200

    Added getters and setters in Product POJO

--- src/com/product/core/Product.java
+++ src/com/product/core/Product.java
@@ -4,5 +4,18 @@ public class Product {
 	
 	private String SKUId;
 	private double price;
+	
+	public String getSKUId() {
+		return SKUId;
+	}
+	public void setSKUId(String sKUId) {
+		SKUId = sKUId;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 }
