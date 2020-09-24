1f4e14b200e90c2c34648d7cc11588a76d2dbba4
Anuradha <anuradhakhadanga6@gmail.com>
2020-09-24 18:07:24 +0200

    Getting the price list per unit for all the products

--- src/com/product/promotion/calculator/PromotionEngineImpl.java
+++ src/com/product/promotion/calculator/PromotionEngineImpl.java
@@ -1,6 +1,8 @@
 package com.product.promotion.calculator;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.product.core.CartItem;
 import com.product.core.Product;
@@ -10,7 +12,12 @@ public class PromotionEngineImpl implements PromotionEngine{
 
 	@Override
 	public double getDiscountPriceOnCart(List<CartItem> cart, List<Promotion> promotions, List<Product> products) {
-		// TODO Auto-generated method stub
+		/*
+		 * Getting the price list per unit for all the products
+		 */
+		Map<String,Double> productPriceMap = products.stream().collect(Collectors.toMap(Product::getSKUId, Product::getPrice));
+		
+		
 		return 0;
 	}
 
